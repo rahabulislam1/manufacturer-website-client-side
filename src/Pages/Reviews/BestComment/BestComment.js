589b6ed0d8be3a600132819681817a2e5ea115fd
rahabulislam1 <rahabulislam1@gmail.com>
2022-05-30 18:50:21 +0600

    Best Review Updated

--- src/Pages/Reviews/BestComment/BestComment.js
+++ src/Pages/Reviews/BestComment/BestComment.js
@@ -4,7 +4,15 @@ const BestComment = () => {
     return (
         <div className='mt-6'>
 
-
+            <div class="hero w-full bg-base-200">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <img src={doctor} class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-5xl font-bold">Best Review</h1>
+                        <p class="py-6">I am Ibrahim.I am a Robotics Engineer. For My project purpose, i need to build some robotics project. I am searching for a trusted supplier.My Friend suggested me their server.I ordered many hardware parts from their site.All the products i received was authentic. I am really very happy for their service.Very much Recommended.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
