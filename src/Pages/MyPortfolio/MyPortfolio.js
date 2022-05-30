0e6dee3c13776cfe5bd027fc1853f44251a8aa5b
rahabulislam1 <rahabulislam1@gmail.com>
2022-05-30 17:31:09 +0600

    Portfolio Updated

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -5,8 +5,8 @@ const MyPortfolio = () => {
         <div>
             <div className="card w-full bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title text-2xl">Razib Debnath</h2>
-                    <p className='text-xl '>Email : <span className='text-success'>Razib.debnath016@gmail.com</span> </p>
+                    <h2 className="card-title text-2xl">Rahabul Islam</h2>
+                    <p className='text-xl '>Email : <span className='text-success'>rahabulislam1@gmail.com</span> </p>
                     <div>
                         <h2 className='text-center text-3xl text-success'>Educational Background</h2>
                         <div className="overflow-x-auto">
@@ -18,14 +18,14 @@ const MyPortfolio = () => {
                                         <th>Institution</th>
                                         <th>Passing year</th>
                                         <th>Group</th>
-                                        <th>GPA</th>
+                                        <th>GPA/CGPA</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <th>1</th>
                                         <td>SSC</td>
-                                        <td>Ideal School and College.</td>
+                                        <td>Nazrul Academy School</td>
                                         <td>2012</td>
                                         <td>Science</td>
                                         <td>5.00</td>
@@ -33,18 +33,18 @@ const MyPortfolio = () => {
                                     <tr>
                                         <th>2</th>
                                         <td>HSC</td>
-                                        <td>Birshrestho Munshi Abdur Rouf Public College</td>
+                                        <td>Agricultural University College</td>
                                         <td>2014</td>
                                         <td>Science</td>
-                                        <td>5.00</td>
+                                        <td>4.90</td>
                                     </tr>
                                     <tr>
                                         <th>3</th>
                                         <td>B.Sc</td>
-                                        <td>Mawlana Bhashani Science And Technology University</td>
+                                        <td>Mawlana Bhashani Science and Technology University</td>
                                         <td>2021</td>
                                         <td>CSE</td>
-                                        <td>3.47</td>
+                                        <td>2.94</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -56,10 +56,10 @@ const MyPortfolio = () => {
                 </div>
             </div>
             <div className='mt-12 text-xl'>
-                <h2 className='text-2xl mb-2'>Three of my Projects link is here :</h2>
-                <p>Thor-Gym : <span className='text-primary'>https://thor-gym-1ba89.firebaseapp.com/</span></p>
-                <p>SmartPhone WareHouse : <span className='text-primary'>https://smartphone-warehouse-d99f2.firebaseapp.com/</span></p>
-                <p>Choose your Shoe : <span className='text-primary'>https://phenomenal-frangipane-e1e5a6.netlify.app/</span></p></div>
+                <h2 className='text-2xl mb-2'>Two of my Projects link is here :</h2>
+                <p>Bike Collection : <span className='text-primary'>https://bike-warehouse2.web.app/</span></p>
+                <p>Capture Your Image : <span className='text-primary'>https://iridescent-arithmetic-521238.netlify.app/</span></p>
+            </div>
         </div>
     );
 };
